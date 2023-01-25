944cc3f047b7f0c67cb6477f58f7c4e247662b87
Trunks Loan <trunkslo@at-q1.msk.21-school.ru>
2023-01-25 17:21:09 +0300

    insert added

--- special_functions/special_functions.c
+++ special_functions/special_functions.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 
 void *s21_to_upper(const char *str) {
-  char* buff = calloc(strlen(str), sizeof(char));
-  for (size_t i = 0; i < s21_strlen(str); i++) {
+  char* buff = calloc(s21_strlen(str), sizeof(char));
+  for (s21_size_t i = 0; i < s21_strlen(str); i++) {
     if (str[i] > 96 && str[i] < 123)
       buff[i] = str[i] - 32;
     else 
@@ -13,11 +13,28 @@ void *s21_to_upper(const char *str) {
 
 void *s21_to_lower(const char *str) {
   char* buff = calloc(s21_strlen(str), sizeof(char));
-  for (size_t i = 0; i < s21_strlen(str); i++) {
+  for (s21_size_t i = 0; i < s21_strlen(str); i++) {
     if (str[i] > 64 && str[i] < 91)
       buff[i] = str[i] + 32;
     else 
       buff[i] = str[i];
+  }
+  return buff;
+}
+
+void *s21_insert(const char *src, const char *str, s21_size_t start_index) {
+  if (start_index > s21_strlen(src))
+    return NULL;
+  char* buff = calloc(s21_strlen(str), sizeof(char));
+  s21_size_t k = 0;
+  for (s21_size_t i = 0; i < s21_strlen(src) + s21_strlen(str); i++) {
+    if (i < start_index) {
+      buff[i] = src[i];
+    } else if (i >= start_index && i < start_index + s21_strlen(str)) {
+      buff[i] = str[k];
+      k++;
+    } else 
+      buff[i] = src[i - s21_strlen(str)];
   }
   return buff;
 }

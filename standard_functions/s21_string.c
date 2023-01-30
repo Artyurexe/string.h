2f6303820588e163d571d951c3c1dcded6db109f
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-01-30 19:34:13 +0300

    strtok

--- standard_functions/s21_string.c
+++ standard_functions/s21_string.c
@@ -148,41 +148,64 @@ char *s21_strstr(const char *haystack, const char *needle) {
 }
 
 char *s21_strtok(char *str, const char *delim) {
-  static char* adr;
-    char* newtok = S21_NULL;
-    char* oldtok = S21_NULL;
-    int i = 0, index_buffer = 0, flag2 = 1;
-    if (str != S21_NULL)
-        adr = str;
-    while (adr[i] != '\0' && flag2) {
-      for (s21_size_t k = 0; k < s21_strlen(delim); k++) {
-        if (adr[i] == delim[k] && !index_buffer) {
-            index_buffer = i;
-          break;
-        }
-        if (k == s21_strlen(delim) - 1 && adr[i] != delim[k] && index_buffer != 0) {
-          flag2 = 0;
-          newtok = adr + i;
-          adr[index_buffer] = '\0';
+  static char *input = S21_NULL;
+  char *ret = S21_NULL;
+  if (str != S21_NULL) {
+    input = str;
+  }
+  if (input != S21_NULL) {
+    s21_size_t delim_len = s21_strlen(delim);
+    int stop = 0;
+    while (!stop && *input) {
+      int is_found_delim = 0;
+      for (s21_size_t i = 0; !is_found_delim && *input && i < delim_len; i++) {
+        if (*input == delim[i]) {
+          is_found_delim = 1;
         }
       }
-      i++;
+      if (!is_found_delim) {
+        stop = 1;
+      } else {
+        input++;
+      }
     }
-    oldtok = adr;
-    adr = newtok;
-    return oldtok;
+    stop = 0;
+    if (*input) {
+      ret = input;
+    }
+    while (!stop && *input) {
+      int is_found_delim = 0;
+      for (s21_size_t i = 0; !is_found_delim && *input && i < delim_len; i++) {
+        if (*input == delim[i]) {
+          is_found_delim = 1;
+          stop = 1;
+          *input = '\0';
+        }
+      }
+      input++;
+    }
+    if (!*input) {
+      input = S21_NULL;
+    }
+  }
+  return ret;
 }
 
 
 int main(){
-    char str[] = "Geeks-for-Geeks";
- 
-    char* token = s21_strtok(str, " - ");
-    
-    while (token != S21_NULL) {
-        printf("%s\n", token);
-        token = s21_strtok(S21_NULL, " - ");
-    }
-
-  return 0;
+  char str[80] = "This is - www.tutorialspoint.com - website";
+  const char s[2] = "-";
+  char *token;
+  
+  /* get the first token */
+  token = s21_strtok(str, s);
+  
+  /* walk through other tokens */
+  while( token != NULL ) {
+    printf("%s\n", token );
+  
+    token = s21_strtok(NULL, s);
+  }
+  
+   return(0);
 }

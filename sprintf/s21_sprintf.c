7af82404d6bc9883e827bc66f5381752e378f377
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-02-09 14:33:21 +0300

    record_char

--- sprintf/s21_sprintf.c
+++ sprintf/s21_sprintf.c
@@ -1,67 +1,54 @@
 #include "s21_sprintf.h"
 
-void s21_sprintf(char *str, const char *format, ...) {
+int s21_sprintf(char *str, const char *format, ...) {
+  int err_num = 0;
   const char* types = "cdieEfgGosuxXpn%%";
   va_list ap;
   char test[100];
   test[0] = '\0';
   va_start(ap, format);
+  s21_size_t spec_num = 0;
   struct specifier spec;
-  for (s21_size_t i = 0; i < s21_strlen(format); i++) {
+  for (s21_size_t i = 0; i < s21_strlen(format) && err_num == 0; i++) {
     specifier_init(&spec);
     if (format[i] == '%') {
-      specifier_parsing((char*) &format[i + 1], &spec);
+      err_num = specifier_parsing((char*) &format[i + 1], &spec);
       i += s21_strcspn(&format[i + 1], types) + 1;
       printf("%s %s %s %s %c\n", spec.flag, spec.length, spec.precision, spec.width, spec.type);
     }
   }
+  return err_num;
 }
 
-void specifier_parsing(char *str, struct specifier* spec) {
+int specifier_parsing(char *str, struct specifier* spec) {
+  int err_num = 0;
   char *buff = malloc(1024);
-  char *buff1 = malloc(1024);
-  int k = 0;
   const char* flags = "-+ #0";
   const char* numbers = "1234567890*";
-  const char* length = "hLl";
+  const char* length = "hlL";
   const char* types = "cdieEfgGosuxXpn%%";
   s21_size_t spec_length = s21_strcspn(str, types) + 1;
   s21_memcpy(buff, str, spec_length);
   spec->type = str[spec_length - 1];
   spec_length = s21_strspn((const char*) buff, flags);
-  s21_memcpy(buff1, buff, spec_length);
-  for (int i = 0; i < 5; i++) {
-    if (s21_strchr(buff1, flags[i]) != S21_NULL)
-      spec->flag[k++] = flags[i];
-  }
-  spec->flag[k] = '\0';
-  k = 0;
+  s21_memcpy(spec->flag, buff, spec_length);
   buff += spec_length;
   spec_length = s21_strspn((const char*) buff, numbers);
   s21_memcpy(spec->width, buff, spec_length);
+  err_num = star_check(spec->width);
   buff += spec_length;
-  if (*(buff + 1) == '.') {
+  if (*(buff + 1) == '.' && err_num == 0) {
     spec_length = s21_strspn((const char*) buff, numbers);
+    err_num = star_check(spec->precision);
     buff += (spec_length + 1);
   }
-  s21_memcpy(buff1, buff, s21_strspn((const char*) buff, length));
-  for (int i = 0; i < 3; i++) {
-    char* c = s21_strchr(buff1, length[i]);
-    if (c != S21_NULL) {
-      if (length[i] != 'l') {
-        spec->length[k++] = length[i];
-      } else if (s21_strchr(c, length[i]) != S21_NULL) {
-        spec->length[k++] = length[i];
-        spec->length[k++] = length[i];
-      } else
-        spec->length[k++] = length[i];
-    }
-  }
-  spec->length[k] = '\0';
-  free(buff);
-  free(buff1);
+  s21_memcpy(spec->length, buff, s21_strspn((const char*) buff, length));
+  return err_num;
 }
 
+int star_check(char* str) {
+  return (s21_strlen(str) != 1 && s21_strchr(str, '*') != S21_NULL);
+}
 
 void vararg_init(char type, va_list *ap) {
   if (type == 'c') {
@@ -110,9 +97,10 @@ void record(char *str, struct specifier spec, va_list *ap) {
 }
 
 void record_char(char *str, struct specifier spec, va_list *ap) {
-  if (s21_strchr(spec.flag, "l"))
-    char c = va_arg(*ap, char);
-  if (s21_strspn(spec.flag, "Ll") != 0)
+  if (s21_strchr(spec.length, 'l')) {
+    wchar_t wc = va_arg(*ap, wchar_t);
+
+  }
 }
 
 int main() {

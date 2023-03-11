db616df30f9c729ed540566a6730abc513cc7b2d
liubavabarsegian <lyubahemmo@mail.ru>
2023-03-12 01:01:58 +0300

    styles

--- sscanf/s21_sscanf.c
+++ sscanf/s21_sscanf.c
@@ -26,19 +26,15 @@ int s21_sscanf(const char *str, const char *format, ...) {
         i++;
       }
       if (format[i] != '%' && (format[i] == string[j])) {
-      }
-      else if (format[i] != '%' && (format[i] != string[j]) && !s21_isspace(string[j])) {
+      } else if (format[i] != '%' && (format[i] != string[j]) &&
+                 !s21_isspace(string[j])) {
         break;
-      }
-      else if (format[i] == '%' && format[i + 1] == '%') {
+      } else if (format[i] == '%' && format[i + 1] == '%') {
         i++;
-      }
-      else if (format[i] == '%' && string[j] == '%')
-      {
+      } else if (format[i] == '%' && string[j] == '%') {
         i++;
         j++;
-      }
-      else if (format[i] && string[j] && format[i] == '%' && string[j]) {
+      } else if (format[i] && string[j] && format[i] == '%' && string[j]) {
         specifier_parsing((char *)&format[i + 1], &spec);
         i += strcspn(&format[i + 1], types) + 1;
         while (s21_isspace(string[j]) && spec.type != 'c') {
@@ -47,38 +43,42 @@ int s21_sscanf(const char *str, const char *format, ...) {
         if (format[i]) {
           c = format[i + 1];
         }
-          success  = match_str_and_format(string, &spec, &ap, &j, c);
-          if (success) {
-            count_successes++;
-            if (spec.width[0] == '*') {
-              count_successes--;
-            }
-          } else if (!success && (spec.type == 'i' || spec.type == 'd' || spec.type == 'u' || spec.type == 'o' || spec.type == 'x' || spec.type == 'X')) {
-            break;
+        success = match_str_and_format(string, &spec, &ap, &j, c);
+        if (success) {
+          count_successes++;
+          if (spec.width[0] == '*') {
+            count_successes--;
           }
+        } else if (!success &&
+                   (spec.type == 'i' || spec.type == 'd' || spec.type == 'u' ||
+                    spec.type == 'o' || spec.type == 'x' || spec.type == 'X')) {
+          break;
+        }
         j--;
       }
       j++;
     }
   }
-  if (string)
-    free(string);
+  if (string) free(string);
   return count_successes;
 }
 
-void specifier_parsing(char *str, struct specifier* spec) {
-  char* buff = str;
-  char* buff1 = malloc(1024);
-  char* buff2 = buff1;
+void specifier_parsing(char *str, struct specifier *spec) {
+  char *buff = str;
+  char *buff1 = malloc(1024);
+  char *buff2 = buff1;
   int k = 0;
-  const char* flags = "-+ #0";
-  const char* numbers = "1234567890*";
-  const char* length = "hlL";
-  const char* types = "cdieEfgGosuxXpn%%";
+  const char *flags = "-+ #0";
+  const char *numbers = "1234567890*";
+  const char *length = "hlL";
+  const char *types = "cdieEfgGosuxXpn%%";
   spec->type = str[strcspn(str, types)];
   pointer_shift(&buff, buff1, flags);
   for (int i = 0; i < 5; i++) {
-    if (strchr(buff1, flags[i]) != NULL && ((flags[i] == ' ' && strchr(buff1, '+') == NULL) || (flags[i] == '0' && strchr(buff1, '-') == NULL) || flags[i] == '-' || flags[i] == '+' || flags[i] == '#'))
+    if (strchr(buff1, flags[i]) != NULL &&
+        ((flags[i] == ' ' && strchr(buff1, '+') == NULL) ||
+         (flags[i] == '0' && strchr(buff1, '-') == NULL) || flags[i] == '-' ||
+         flags[i] == '+' || flags[i] == '#'))
       spec->flag[k++] = flags[i];
   }
   spec->flag[k] = '\0';
@@ -93,38 +93,42 @@ void specifier_parsing(char *str, struct specifier* spec) {
     else
       numbers_parsing(spec->precision, buff1);
   }
-  memcpy(buff1, buff, strspn((const char*) buff, length));
+  memcpy(buff1, buff, strspn((const char *)buff, length));
   for (int i = 0; i < 3; i++) {
-    char* c = strchr(buff1, length[i]);
+    char *c = strchr(buff1, length[i]);
     if (c != NULL) {
-      if (length[i] == 'L' || (length[i] == 'h' && (c < strchr(buff1, 'l') || strchr(buff1, 'l') == NULL)) || (length[i] == 'l' && strchr(c + 1, length[i]) == NULL && spec->length[0] != 'h')) {
+      if (length[i] == 'L' ||
+          (length[i] == 'h' &&
+           (c < strchr(buff1, 'l') || strchr(buff1, 'l') == NULL)) ||
+          (length[i] == 'l' && strchr(c + 1, length[i]) == NULL &&
+           spec->length[0] != 'h')) {
         spec->length[k++] = length[i];
-      } else if (length[i] == 'l' && strchr(c + 1, length[i]) != NULL && spec->length[0] != 'h') {
+      } else if (length[i] == 'l' && strchr(c + 1, length[i]) != NULL &&
+                 spec->length[0] != 'h') {
         spec->length[k++] = length[i];
         spec->length[k++] = length[i];
-      } 
+      }
     }
   }
   spec->length[k] = '\0';
   free(buff2);
 }
 
-void numbers_parsing(char* str, char* buff) {
-  size_t length = strspn((const char*) buff, "1234567890");
-  if (*buff == '*' && length == 0)
-    length = 1;
+void numbers_parsing(char *str, char *buff) {
+  size_t length = strspn((const char *)buff, "1234567890");
+  if (*buff == '*' && length == 0) length = 1;
   strcpy(str, buff);
   buff[length + 1] = '\0';
 }
 
-void pointer_shift(char** buff, char* buff1, const char* str) {
-  size_t length = strspn((const char*) *buff, str);
+void pointer_shift(char **buff, char *buff1, const char *str) {
+  size_t length = strspn((const char *)*buff, str);
   strncpy(buff1, *buff, length);
   buff1[length] = '\0';
   *buff += length;
 }
 
-void specifier_init(struct specifier* spec) {
+void specifier_init(struct specifier *spec) {
   spec->flag[0] = '\0';
   spec->width[0] = '\0';
   spec->precision[0] = '\0';
@@ -132,13 +136,12 @@ void specifier_init(struct specifier* spec) {
   spec->type = 0;
 }
 
-int read_d(char *str, va_list *ap, struct specifier *spec, int *j, char c)
-{
+int read_d(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int success = 0;
   int i = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%' && s21_isdigit(str[*j +i])) {
-    if (i > 0 && (str[*j + i] == '-' || str[*j + i] == '+'))
-    {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%' && s21_isdigit(str[*j + i])) {
+    if (i > 0 && (str[*j + i] == '-' || str[*j + i] == '+')) {
       break;
     }
     i++;
@@ -151,26 +154,24 @@ int read_d(char *str, va_list *ap, struct specifier *spec, int *j, char c)
   copy[i] = '\0';
   if (spec->width[0] != '*') {
     if (strcmp(spec->length, "l") == 0) {
-      
       long int *d = va_arg(*ap, long int *);
       *d = atol(copy);
-    }
-    else if (strcmp(spec->length, "ll") == 0){
+    } else if (strcmp(spec->length, "ll") == 0) {
       long long *d = va_arg(*ap, long long *);
       *d = atoll(copy);
-    }
-    else if (strcmp(spec->length,"h") == 0) {
+    } else if (strcmp(spec->length, "h") == 0) {
       short *d = va_arg(*ap, short *);
       *d = atoi(copy);
-    }
-    else {
+    } else {
       int *d = va_arg(*ap, int *);
       *d = atoi(copy);
     }
-      if ((copy) && (atol(copy) || atoll(copy) || atoi(copy) || ((strcmp(copy, "0") == 0 || strcmp(copy, "-0") == 0 || strcmp(copy, "+0") == 0) && atoi(copy) == 0)))
-        success = 1;  
-  } 
-  else if (copy) {
+    if ((copy) && (atol(copy) || atoll(copy) || atoi(copy) ||
+                   ((strcmp(copy, "0") == 0 || strcmp(copy, "-0") == 0 ||
+                     strcmp(copy, "+0") == 0) &&
+                    atoi(copy) == 0)))
+      success = 1;
+  } else if (copy) {
     success = 1;
   }
   free(copy);
@@ -187,10 +188,12 @@ int s21_isdigit(char c) {
 }
 
 int s21_ishex(char c) {
-  return (c >= '0' && c <= '9' || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') || c == 'x' || c == 'X' || c == '-' || c == '+');
+  return (c >= '0' && c <= '9' || (c >= 'a' && c <= 'f') ||
+          (c >= 'A' && c <= 'F') || c == 'x' || c == 'X' || c == '-' ||
+          c == '+');
 }
 
-int s21_isoctal(char c)  {
+int s21_isoctal(char c) {
   return (c >= '0' && c <= '7' || c == '-' || c == '+');
 }
 
@@ -198,18 +201,16 @@ int s21_empty_str(char *str) {
   int found_not_space = 1;
   int i = 0;
   while (str[i]) {
-    if (!s21_isspace(str[i]))
-      found_not_space = 0;
+    if (!s21_isspace(str[i])) found_not_space = 0;
     i++;
   }
   return found_not_space;
 }
 
-int read_s(char *str, va_list *ap, struct specifier *spec, int *j)
-{
+int read_s(char *str, va_list *ap, struct specifier *spec, int *j) {
   int success = 0;
   int i = 0;
-  while (str[*j + i] != '\0'  && !s21_isspace(str[*j + i])) {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i])) {
     i++;
   }
   if (atoi(spec->width) < i && atoi(spec->width) != 0) {
@@ -222,22 +223,19 @@ int read_s(char *str, va_list *ap, struct specifier *spec, int *j)
 
     char *new_str = va_arg(*ap, char *);
     strcpy(new_str, copy);
-    if (new_str)
-      success++;
+    if (new_str) success++;
     free(copy);
   }
   *j += i;
   return success;
 }
 
-int read_c(char *str, va_list *ap, struct specifier *spec, int *j)
-{
+int read_c(char *str, va_list *ap, struct specifier *spec, int *j) {
   int success = 0;
   if (spec->width[0] != '*') {
     char *c = va_arg(*ap, char *);
     *c = str[*j];
-    if (c)
-      success = 1;
+    if (c) success = 1;
   }
   (*j)++;
   return success;
@@ -245,8 +243,9 @@ int read_c(char *str, va_list *ap, struct specifier *spec, int *j)
 
 int read_u(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int success = 0;
-  int i  = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%' && s21_isdigit(str[*j +i])) {
+  int i = 0;
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%' && s21_isdigit(str[*j + i])) {
     i++;
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -266,20 +265,19 @@ int read_u(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
       unsigned int *u = va_arg(*ap, unsigned int *);
       *u = strtoul(copy, '\0', 10);
     }
-    if (strtoul(copy, '\0', 10)|| (copy[0] == '0'))
-      success = 1;
+    if (strtoul(copy, '\0', 10) || (copy[0] == '0')) success = 1;
     free(copy);
   }
   *j += i;
-  return success; 
+  return success;
 }
 
 int read_o(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int i = 0;
   int success = 0;
-  while (str[*j] == '0')
-    (*j)++;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%' && s21_isoctal(str[*j + i])) {
+  while (str[*j] == '0') (*j)++;
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%' && s21_isoctal(str[*j + i])) {
     i++;
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -287,42 +285,37 @@ int read_o(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   }
   if (spec->width[0] != '*') {
     if (str[*j - 1] == '0' && i == 0) {
-      //i = 1;
+      // i = 1;
       success++;
     }
     char *copy = malloc(i + 1);
     strncpy(copy, str + *j, i);
     copy[i] = '\0';
     if (strcmp(spec->length, "l") == 0) {
-      
       long int *d = va_arg(*ap, long int *);
       *d = strtol(copy, (char **)NULL, 8);
-    }
-    else if (strcmp(spec->length, "ll") == 0){
+    } else if (strcmp(spec->length, "ll") == 0) {
       long long *d = va_arg(*ap, long long *);
       *d = strtoll(copy, (char **)NULL, 8);
-    }
-    else if (strcmp(spec->length,"h") == 0) {
+    } else if (strcmp(spec->length, "h") == 0) {
       short *d = va_arg(*ap, short *);
       *d = strtol(copy, (char **)NULL, 8);
-    }
-    else {
+    } else {
       int *d = va_arg(*ap, int *);
       *d = strtol(copy, (char **)NULL, 8);
     }
-    if (strtol(copy, (char **)NULL, 8) || (copy[0] == '0'))
-      success = 1;
+    if (strtol(copy, (char **)NULL, 8) || (copy[0] == '0')) success = 1;
     free(copy);
   }
   *j += i;
-  return success; 
+  return success;
 }
 
-int read_xX(char *str, va_list *ap, struct specifier *spec, int *j, char c) 
-{
+int read_xX(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int i = 0;
   int success = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%' && s21_ishex(str[*j + i])) {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%' && s21_ishex(str[*j + i])) {
     i++;
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -343,19 +336,19 @@ int read_xX(char *str, va_list *ap, struct specifier *spec, int *j, char c)
       unsigned int *x = va_arg(*ap, unsigned int *);
       *x = strtol(copy, (char **)NULL, 16);
     }
-    //if X?????how to make upper case
-    if (strtol(copy, (char **)NULL, 16)|| (copy[0] == '0'))
-      success = 1;
+    // if X?????how to make upper case
+    if (strtol(copy, (char **)NULL, 16) || (copy[0] == '0')) success = 1;
     free(copy);
   }
   *j += i;
-  return success; 
+  return success;
 }
 
 // int read_e(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
 //   int i = 0;
 //   int success = 0;
-//   while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%') {
+//   while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c
+//   && str[*j + i] != '%') {
 //     i++;
 //   }
 //   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -380,13 +373,14 @@ int read_xX(char *str, va_list *ap, struct specifier *spec, int *j, char c)
 //     free(copy);
 //   }
 //   *j += i;
-//   return success; 
+//   return success;
 // }
 
-int read_f(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
+int read_f(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int i = 0;
   int success = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%') {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%') {
     i++;
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -396,26 +390,25 @@ int read_f(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
     char *copy = malloc(i + 1);
     strncpy(copy, str + *j, i);
     copy[i] = '\0';
-    if (strcmp(spec->length, "L") == 0)
-    {
+    if (strcmp(spec->length, "L") == 0) {
       long double *f = va_arg(*ap, long double *);
       *f = atof(copy);
     } else {
       float *f = va_arg(*ap, float *);
       *f = atof(copy);
     }
-    if (atof(copy)|| (copy[0] == '0'))
-      success = 1;
+    if (atof(copy) || (copy[0] == '0')) success = 1;
     free(copy);
   }
   *j += i;
-  return success; 
+  return success;
 }
 
-int read_g(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
+int read_g(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int i = 0;
   int success = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%') {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%') {
     i++;
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -427,18 +420,18 @@ int read_g(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
     copy[i] = '\0';
     float *f = va_arg(*ap, float *);
     *f = atof(copy);
-    if (strtol(copy, (char **)NULL, 16)|| (copy[0] == '0'))
-      success = 1;
+    if (strtol(copy, (char **)NULL, 16) || (copy[0] == '0')) success = 1;
     free(copy);
   }
   *j += i;
-  return success; 
+  return success;
 }
 
-int read_e(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
+int read_e(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int i = 0;
   int success = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%') {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%') {
     i++;
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
@@ -450,20 +443,19 @@ int read_e(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
     copy[i] = '\0';
     float *f = va_arg(*ap, float *);
     *f = atof(copy);
-    if (strtol(copy, (char **)NULL, 16)|| (copy[0] == '0'))
-      success = 1;
+    if (strtol(copy, (char **)NULL, 16) || (copy[0] == '0')) success = 1;
     free(copy);
   }
   *j += i;
-  return success; 
+  return success;
 }
 
-int read_i(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
+int read_i(char *str, va_list *ap, struct specifier *spec, int *j, char c) {
   int i = 0;
   int success = 0;
-  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c && str[*j + i] != '%') {
+  while (str[*j + i] != '\0' && !s21_isspace(str[*j + i]) && str[*j + i] != c &&
+         str[*j + i] != '%') {
     i++;
-    
   }
   if (atoi(spec->width) && atoi(spec->width) < i) {
     i = atoi(spec->width);
@@ -485,65 +477,66 @@ int read_i(char *str, va_list *ap, struct specifier *spec,  int *j, char c) {
         success = read_o(str, ap, spec, j, c);
       }
     }
-    if (atoll(copy) || strtol(copy, (char **)NULL, 16) ||  strtol(copy, (char **)NULL, 8)|| (copy[0] == '0'))
+    if (atoll(copy) || strtol(copy, (char **)NULL, 16) ||
+        strtol(copy, (char **)NULL, 8) || (copy[0] == '0'))
       success = 1;
     free(copy);
   }
-  return success; 
+  return success;
 }
 
-int match_str_and_format(char *str, struct specifier *spec, va_list *ap, int *j, char c) {
+int match_str_and_format(char *str, struct specifier *spec, va_list *ap, int *j,
+                         char c) {
   int success = 0;
   switch (spec->type) {
-    case 'd': ;
+    case 'd':;
       success = read_d(str, ap, spec, j, c);
       break;
-    case 'c': ;
+    case 'c':;
       success = read_c(str, ap, spec, j);
       break;
-    case 'i': ;
+    case 'i':;
       success = read_i(str, ap, spec, j, c);
       break;
-    case 'e': 
-    success = read_e(str, ap, spec, j, c);
+    case 'e':
+      success = read_e(str, ap, spec, j, c);
       break;
-    case 'E': ;
-    success = read_e(str, ap, spec, j, c);
+    case 'E':;
+      success = read_e(str, ap, spec, j, c);
       break;
-    case 'f': ;
+    case 'f':;
       success = read_f(str, ap, spec, j, c);
       break;
-    case 'g': 
-    success = read_g(str, ap, spec, j, c);
+    case 'g':
+      success = read_g(str, ap, spec, j, c);
       break;
-    case 'G': ;
-    success = read_g(str, ap, spec, j, c);
+    case 'G':;
+      success = read_g(str, ap, spec, j, c);
       break;
-    case 'o': 
+    case 'o':
       success = read_o(str, ap, spec, j, c);
       break;
-    case 's': ;
+    case 's':;
       success = read_s(str, ap, spec, j);
       break;
-    case 'u': ;
+    case 'u':;
       success = read_u(str, ap, spec, j, c);
       break;
-    case 'x': ;
+    case 'x':;
       success = read_xX(str, ap, spec, j, c);
       break;
-    case 'X': ;
+    case 'X':;
       success = read_xX(str, ap, spec, j, c);
       break;
-    case 'p': ;
+    case 'p':;
       break;
-    case 'n': ;
+    case 'n':;
       int *n = va_arg(*ap, int *);
       *n = *j;
       break;
   }
   return success;
 }
-
 
 // int main() {
 //   char fstr[] = "%d%d%%%d%d";
